eec7801fe11503b89a9b03fba3318611ec9acf06
Ty Sayers <tasayers@uwaterloo.ca>
2017-04-28 19:07:01 -0400

    - Fix bug in convertReplay.sh script where it would attempt to convert already-converted files, or files that already had a converted counterpart format.

--- convertReplays.sh
+++ convertReplays.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 # Grab all files in the specified directory in command line arguments
-FILES=$1/*
+FILES=$1/*.gior
 
 # Convert each file from its .gior format to .gioreplay format.
 for f in $FILES
 do
-  echo "Converting $f file into .gioreplay format..."
-  node ./generals.io-Replay-Utils/converter.js $f
+  CONVERTED_NAME="${f}eplay"
+  if [ ! -f ${CONVERTED_NAME} ]; then
+    echo "Converting $f file into .gioreplay format..."
+    node ./generals.io-Replay-Utils/converter.js $f
+  fi
 done
